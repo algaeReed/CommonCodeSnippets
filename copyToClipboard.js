9b53a050ee0db51c6b20c16c098b998715062c42
芦苇 <30750777+algaeReed@users.noreply.github.com>
2023-03-16 17:43:22 +0800

    Create copyToClipboard.js

---
+++ copyToClipboard.js
@@ -0,0 +1,23 @@
+const copyToClipboard = (text = "text") => {
+  if (navigator.clipboard) {
+    // clipboard api 复制
+    navigator.clipboard.writeText(text);
+    alert('复制成功');
+  } else {
+    var textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    // 隐藏此输入框
+    textarea.style.position = 'fixed';
+    textarea.style.clip = 'rect(0 0 0 0)';
+    textarea.style.top = '10px';
+    // 赋值
+    textarea.value = text;
+    // 选中
+    textarea.select();
+    // 复制
+    document.execCommand('copy', true);
+    // 移除输入框
+    document.body.removeChild(textarea);
+    alert('复制成功')
+  }
+}
